e6b59e9ad26b5fee89d85ae9d155923ace55f09c
Catherine O'Hara <catherine.ohara@outlook.com>
2019-07-03 04:34:04 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -31,36 +31,49 @@ def consolidate_cart(cart)
 end
 
 
+
 def apply_coupons(cart, coupons)
   coupon_hash = {}
   if coupons.nil?
     return cart
-  else
-    coupons.each do |hash|
-      value_price = hash[:cost]/hash[:num] #calculate
-      
-      item_hash = {
-        "#{hash[:item]} W/COUPON" => {
-          :price => value_price,
-          :clearance => true,
-          :count => hash[:num]
-        }
-      }
-      
+  else 
+    coupons.each do |coup_hash|
+        value_price = coup_hash[:cost]/coup_hash[:num] #calculate
+        item = coup_hash[:item]
+          if coupons.length >= 1 && cart.include?(item)
+            item_hash = {
+              "#{coup_hash[:item]} W/COUPON" => {
+                :price => value_price,
+                :clearance => cart[item][:clearance],
+                :count => coup_hash[:num]
+              }
+            }
+          else
+            coupons.length >= 1 && !cart.include?(item)
+            coupons -= coup_hash
+          end
+          
       cart.each do |food, food_hash| 
-        food_hash[:count] -= hash[:num]
+        food_hash[:count] -= coup_hash[:num]
       end
-
+      
       final = coupon_hash.merge!(item_hash)
       cart.merge!(final)
     end
   end 
-
   return cart
 end
 
+
+
 def apply_clearance(cart)
-  # code here
+  cart.each do |item, item_hash|
+    if item_hash[:clearance] == true 
+      discount = item_hash[:price] * 0.20
+      item_hash[:price] -= discount
+    end
+  end
+  return cart 
 end
 
 def checkout(cart, coupons)
